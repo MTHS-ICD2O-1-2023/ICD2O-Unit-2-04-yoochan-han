e3276a7eed22811024048d7747364aef9178e4a2
yoochan han <159185007+yoochan-han@users.noreply.github.com>
2024-03-21 23:22:30 +0000

    c

--- js/script.js
+++ js/script.js
@@ -1,22 +1,19 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Yoochan Han
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
-function defUnit() {
-  let unit = document.getElementById("get_unit").value
+function CaculateToClick() {
+  //input the element
+  let unit = String(document.getElementById("get_unit").value)
   if (unit == "") {
     let unit = "cm"
   }
-}
-
-function CaculateToClick() {
-  //input the element
-  let h = int(document.getElementById("get_height").value)
-  let b = int(document.getElementById("get_base").value)
+  let h = parseInt(document.getElementById("get_height").value)
+  let b = parseInt(document.getElementById("get_base").value)
   //process
-  let area = (h*b)%2
+  let area = (h*b)/2
   //output
-  document.getElementById("result").innerHTML = 
-  "The area of a triangle with base " + b + unit + "and height " + h + unit + " is " (h*b)%2 + unit + "²"
+  document.getElementById("Result").innerHTML = 
+  ("The area of a triangle with base " + b + unit + " and height " + h + unit + " is " + area + unit + "²")
 }
